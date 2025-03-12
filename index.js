5a5ebb2e730bb3345f10a4ffaafccfd3e072e786
I-GIKARU <aizohgikaru@gmail.com>
2025-03-12 21:53:38 +0300

    completed

---
+++ index.js
@@ -0,0 +1,16 @@
+// let a = 10;
+// let b = 5;
+
+// Function expression for divide
+const divide = function(a, b) {
+  return a / b;
+};
+
+// Arrow function for square
+const square = x => x * x;
+
+// Arrow function for add
+const add = (a, b) => a + b;
+// console.log(divide(a, b));
+// console.log(square(a));
+// console.log(add(a, b));
